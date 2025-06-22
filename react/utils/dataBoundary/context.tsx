c5f892d3e6e553ba9c0acd2025cd59aaa4803f7f
b0rskii <gldimon@mail.ru>
2025-06-22 16:06:05 +0700

    feat: обновлен dataBoundary

--- react/utils/dataBoundary/context.tsx
+++ react/utils/dataBoundary/context.tsx
@@ -1,18 +1,13 @@
 import {
   createContext,
   PropsWithChildren,
-  useCallback,
   useContext,
   useLayoutEffect,
-  useMemo,
   useRef,
   useState,
 } from 'react';
 
-type Errors = Record<string, true>;
-
 type Params = {
-  errors: Errors;
   notification: string;
   updateErrors: (name: string, value: boolean) => void;
 };
@@ -20,44 +15,43 @@ type Params = {
 const DataBoundaryContext = createContext<Params | null>(null);
 export const useDataBoundary = () => useContext(DataBoundaryContext) as Params;
 
-type ProviderProps = PropsWithChildren<{
+type DataBoundaryProps = PropsWithChildren<{
   data: unknown;
-  onStatusChange: (status: boolean) => void;
+  onDataChange: (isValid: boolean) => void;
 }>;
 
 export const DataBoundary = ({
   data,
   children,
-  onStatusChange,
-}: ProviderProps) => {
-  const [errors, setErrors] = useState<Errors>({});
-  const errorsRef = useRef<Errors>({});
-
-  const notification = useMemo(() => {
-    const errorFields = Object.keys(errors).join(', ');
-    if (!errorFields) return '';
-
-    return `Не удалось получить данные: ${errorFields.toLowerCase()}`;
-  }, [errors]);
+  onDataChange,
+}: DataBoundaryProps) => {
+  const [notification, setNotification] = useState('');
+  const errorsRef = useRef<Record<string, true>>({});
 
   useLayoutEffect(() => {
-    setErrors({ ...errorsRef.current });
-    onStatusChange(Object.keys(errorsRef.current).length === 0);
+    const errorFields = Object.keys(errorsRef.current).join(', ');
+
+    setNotification(
+      errorFields
+        ? `Не удалось получить данные: ${errorFields.toLowerCase()}.`
+        : '',
+    );
+
+    onDataChange(!errorFields);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
 
-  const updateErrors = useCallback<Params['updateErrors']>((name, value) => {
+  const updateErrors: Params['updateErrors'] = (name, value) => {
     if (value) {
       errorsRef.current[name] = true;
       return;
     }
     delete errorsRef.current[name];
-  }, []);
+  };
 
   return (
     <DataBoundaryContext.Provider
       value={{
-        errors,
         notification,
         updateErrors,
       }}
